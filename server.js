fdf2532890f1a497fd1babebcf976282fccd7b94
matt fyke <mfyke@live.com>
2018-12-19 13:28:54 -0800

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -17,6 +18,16 @@ const connection = mongoose.connect(MONGODB_URI);
 // use routes
 
 app.use(items);
+
+// Server static assets if in production
+
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 8000;
 
